6d4099191406198e4de204e72861f4c758c148f8
Shahrin Shahrulzaman <shahrin14@gmail.com>
2018-10-02 15:12:24 +0800

    added navigation

--- public/js/event.js
+++ public/js/event.js
@@ -1,10 +1,4 @@
-
-
 //HTML5 audio, will need user touch input to start on mobile
-
-
-
-
 
 AFRAME.registerComponent('markerhandler', {
     init: function () {
@@ -23,8 +17,15 @@ AFRAME.registerComponent('markerhandler', {
                 "1": {
                     "name": "JamesBanbury_WSSW_singing_test_edit.mp3",
                     "format": "mp3"
+                },
+                "2": {
+                    "name": "PeteDavis_WSSW_drum_test_edit.mp3",
+                    "format": "mp3"
+                },
+                "3": {
+                    "name": "ThierryHalbroth_WSSW_flute_test_edit.mp3",
+                    "format": "mp3"
                 }
-
             };
 
         for (var key in data) {
@@ -38,48 +39,61 @@ AFRAME.registerComponent('markerhandler', {
         var markerList = document.querySelectorAll(".markers");
 
         markerList.forEach(function (marker, index) {
-            marker.addEventListener('markerFound', function () {
-                console.log('Marker ' + index + ' found');
-                intro[index].play();
+            marker.setAttribute('link', {
+                href: "https://www.google.com",
+                visualAspectEnabled: false
             });
-            marker.addEventListener('markerLost', (e) => {
-                console.log('Marker ' + index + ' lost');
-                intro[index].pause();
-                intro[index].currentTime = 0;
-            });
-
+            try {
+                marker.addEventListener('markerFound', function (ev) {
+                    console.log('Marker ' + index + ' found');
+                    intro[index].play();
+                });
+                marker.addEventListener('markerLost', function (ev) {
+                    console.log('Marker ' + index + ' lost');
+                    intro[index].pause();
+                    intro[index].currentTime = 0;
+                });
+                marker.addEventListener('click', function (ev) {
+                    if (marker.object3D.visible == true && ev.detail.cursorEl) {
+                        console.log('Marker clicked');
+                    }
+                });
+            }
+            catch (e) {
+                console.log(e);
+            }
         });
 
         // Set up the tick throttling. Will check if marker is active every 500ms
         //this.tick = AFRAME.utils.throttleTick(this.tick, 500, this);
 
-    },
-
-    tick: function (t, dt) {
-        /*
-        var els = sceneEl.querySelectorAll('*');
-        for (var i = 0; i < els.length; i++) {
-            console.log(els[i]);
-        }
-        if (document.querySelector(".markers").object3D.visible == true) {
-
-            /*
-            if (document.querySelector(".markers").getAttribute('value') != null) {
-                if (data.playing == false) {
-                    // MARKER IS PRESENT
-                    intro.play();
-                    playing = true;
-                    console.log("test");
-                }
-            }
-            
-        }
-        else if (document.querySelector(".markers").object3D.visible == false) {
-            intro.pause();
-            intro.currentTime = 0;
-            playing = false;
-        }
-
-        */
     }
+
+    //tick: function (t, dt) {
+    /*
+    var els = sceneEl.querySelectorAll('*');
+    for (var i = 0; i < els.length; i++) {
+        console.log(els[i]);
+    }
+    if (document.querySelector(".markers").object3D.visible == true) {
+
+        /*
+        if (document.querySelector(".markers").getAttribute('value') != null) {
+            if (data.playing == false) {
+                // MARKER IS PRESENT
+                intro.play();
+                playing = true;
+                console.log("test");
+            }
+        }
+        
+    }
+    else if (document.querySelector(".markers").object3D.visible == false) {
+        intro.pause();
+        intro.currentTime = 0;
+        playing = false;
+    }
+
+    */
+    //}
 });
